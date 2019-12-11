10ca62da56871e4a6c22fda2bc72aeebd4f6832b
E. Choroba <choroba@matfyz.cz>
2019-12-11 10:01:03 +0100

    Solve 11/2

---
+++ 11b.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use feature qw{ say };
+
+use enum qw( UP RIGHT DOWN LEFT );
+
+use lib '.';
+use Intcode;
+
+my %panel = ('0:0' => 1);
+my ($x, $y) = (0, 0);
+my $direction = UP;
+
+my $intcode = 'Intcode'->new;
+$intcode->load(split /,/, <>);
+$intcode->pause(1);
+
+until ($intcode->finished) {
+    $intcode->input($panel{"$x:$y"} // 0);
+    $intcode->run;
+    my $paint = $intcode->output->[-1];
+
+    $intcode->run;
+    my $turn = $intcode->output->[-1];
+
+    $panel{"$x:$y"} = $paint;
+    $direction += (-1, 1)[$turn];
+    $direction %= 4;
+    my $step = { (UP)    => [ 0, -1],
+                 (RIGHT) => [ 1,  0],
+                 (DOWN)  => [ 0,  1],
+                 (LEFT)  => [-1,  0],
+               }->{$direction};
+    $x += $step->[0];
+    $y += $step->[1];
+}
+my @grid;
+for (map [split /:/], grep $panel{$_}, keys %panel) {
+    $grid[ $_->[1] ][ $_->[0] ] = 1;
+}
+for my $line (@grid) {
+    say map $_ ? '#' : ' ', @$line;
+}
