b27879b18046358e8b149016e6cc282aabd77f62
E. Choroba <choroba@matfyz.cz>
2019-12-19 08:51:13 +0100

    Solve 19/1

---
+++ 19a.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use feature qw{ say };
+
+use lib '.';
+use Intcode;
+
+my $intcode = 'Intcode'->new;
+my $src = <>;
+my $sum = 0;
+for my $x (0 .. 49) {
+    for my $y (0 .. 49) {
+        $intcode->load(split /,/, $src);
+        $intcode->input($x, $y);
+        $intcode->run;
+        $sum += $intcode->output->[-1];
+        $intcode->flush;
+    }
+}
+say $sum;
