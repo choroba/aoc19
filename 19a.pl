e5240a75c6fd7af58717c3038c59c12eaad1a113
E. Choroba <choroba@matfyz.cz>
2019-12-19 10:53:25 +0100

    Optimize 19/1

--- 19a.pl
+++ 19a.pl
@@ -7,11 +7,11 @@ use lib '.';
 use Intcode;
 
 my $intcode = 'Intcode'->new;
-my $src = <>;
+my @src = split /,/, <>;
 my $sum = 0;
 for my $x (0 .. 49) {
     for my $y (0 .. 49) {
-        $intcode->load(split /,/, $src);
+        $intcode->load(@src);
         $intcode->input($x, $y);
         $intcode->run;
         $sum += $intcode->output->[-1];
